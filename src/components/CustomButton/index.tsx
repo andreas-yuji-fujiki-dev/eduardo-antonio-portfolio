6bf3b1e1327f76c4792f0ad9b33e70eeb349b84d
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-09 09:08:09 -0300

    feat(CustomButton): base for different variant styles

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,27 +1,70 @@
+'use client';
+
+// stores
+import { useThemeStore } from "@/stores/themeStore";
+
 // types
 import { CustomButtonTypes } from "@/types/components/CustomButtonTypes";
 
-// custom button component
-export default function CustomButton({ 
-    icon,
-    link,
-    variant, 
-    children, 
-    backgroundColor,
-}: CustomButtonTypes){
-    return(
-        <a href={link && link} target="_blank">
-            <button style={ backgroundColor ? { backgroundColor: backgroundColor } : {} }>
-                {/* icon */}
-                {icon && (
-                    <div>
-                        {icon}
-                    </div>
-                )}
-
-                {/* button content */}
-                {children}
-            </button>
-        </a>
-    );
+// estilos por variant
+const variantStyles: Record<CustomButtonTypes["variant"], string> = {
+  default: "px-4 py-2 rounded shadow-md",
+  'no-icon': "px-4 py-2 rounded border border-gray-400",
+  transparent: "bg-transparent text-inherit hover:bg-gray-100",
+  'just-icon': "",
 };
+
+// component
+export default function CustomButton({ 
+  icon,
+  link,
+  variant, 
+  children, 
+  backgroundColor,
+  className
+}: CustomButtonTypes) {
+  const isDarkTheme = useThemeStore((state) => state.theme === 'dark');
+
+  const themeStyle = isDarkTheme 
+    && variant !== 'just-icon' 
+    && variant !== 'transparent'
+        ? 'bg-white text-black' 
+        : 'bg-black text-white';
+
+  const combinedClassName = `
+    flex items-center justify-center
+    cursor-pointer
+    ${themeStyle}
+    ${variantStyles[variant]}
+    ${className ?? ''}
+  `.trim();
+
+  const buttonElement = (
+    <button
+      className={combinedClassName}
+      style={backgroundColor ? { backgroundColor } : {}}
+    >
+      {/* icon */}
+      {icon && (
+        <div className={`flex items-center ${variant !== 'just-icon' && variant !== 'no-icon' ? 'mr-2' : ''}`}>
+          {icon}
+        </div>
+      )}
+
+      {/* text */}
+      {variant !== 'just-icon' && (
+        <span className="flex items-center">
+          {children}
+        </span>
+      )}
+    </button>
+  );
+
+  return link ? (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      {buttonElement}
+    </a>
+  ) : (
+    buttonElement
+  );
+}
