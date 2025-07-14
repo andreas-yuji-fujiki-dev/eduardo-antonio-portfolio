c2dc453e0654f216c982edebd171478e519d74ac
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-14 16:32:43 -0300

    feat: create app footer component

--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -1,7 +1,38 @@
+import SocialButtons from "../SocialButtons";
+import SocialButtonsPathsJson from "@/constants/SocialButtonsPaths.json";
+
 export default function AppFooter(){
+    const currentYear = new Date().getFullYear();
+    const githubLink = SocialButtonsPathsJson[0].path;
+
     return (
-        <footer>
-            
+        <footer
+            className={`
+                flex
+                justify-around
+                py-10
+                border-t-1
+            `}
+        >
+            <span
+                className={`
+
+                `}
+            >
+                <a href={ githubLink }>
+                    &lt;/&gt; .edward
+                </a>
+            </span>
+
+            <SocialButtons />
+
+            <span
+                className={`
+                    
+                `}
+            >
+                &copy;{ currentYear }
+            </span>
         </footer>
     );
 };
