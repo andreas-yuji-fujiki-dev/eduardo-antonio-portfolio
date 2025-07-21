722c6f3dc62985e2a69d58f170dc5e53b1fe2a4e
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-21 04:25:39 -0300

    feat: stacks router base

---
+++ backend/src/routes/private/stacks.ts
@@ -0,0 +1,17 @@
+// imports
+    // express
+    import { Router } from "express";
+    
+    // middlewares
+
+    // controllers
+
+// router config
+    // definition
+    const stacksRouter = Router();
+    
+    // registering routes
+    // stacksRouter.get('/', middleware1, middleware2, controller);
+
+// exporting
+export default stacksRouter;
