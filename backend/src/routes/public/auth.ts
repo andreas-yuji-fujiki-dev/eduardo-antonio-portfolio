27e27c721eb48c080e086fe8eef514a9c319f218
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-21 04:25:45 -0300

    feat: auth router

---
+++ backend/src/routes/public/auth.ts
@@ -0,0 +1,22 @@
+// imports
+    // express
+    import { Router } from "express";
+
+    // middlewares
+    import authFieldsMiddleware from "../../middlewares/auth/authFieldsMiddleware";
+
+    // controllers
+    import userRegisterController from "../../controllers/auth/userRegisterController";
+    import userLoginController from "../../controllers/auth/userLoginController";
+
+// router config
+    // definition
+    const authRouter = Router();
+
+    // registering routes
+        // register
+        authRouter.post('/register', authFieldsMiddleware, userRegisterController)
+        // login
+        authRouter.post('/login', authFieldsMiddleware, userLoginController)
+
+export default authRouter;
