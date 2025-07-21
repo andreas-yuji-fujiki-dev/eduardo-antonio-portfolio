932e1e184d5ece8018040babe03c0a202c24c746
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-21 04:23:05 -0300

    feat: app structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,33 +1,37 @@
+import projectsRouter from './routes/private/projects';
+import stacksRouter from './routes/private/stacks';
+import authRouter from './routes/public/auth';
+
 import express, { Application } from 'express';
+import { AppTypes } from './types/AppTypes';
 
 export class App {
   public app: Application;
   private port: number;
 
-  constructor(port: number = 3000) {
+  constructor( { port = 3000 } : AppTypes ) {
     this.app = express();
     this.port = port;
 
-    this.middlewares();
-    this.routes();
-  }
-
-  private middlewares(): void {
     this.app.use(express.json());
-  }
 
-  private routes(): void {
-    
-  }
+    this.routers();
+  };
+
+  private routers(): void {
+    this.app.use('/projects', projectsRouter)
+    this.app.use('/stacks', stacksRouter)
+    this.app.use('/auth', authRouter)
+  };
 
   public listen(): void {
     this.app.listen(this.port, () => {
       console.log(`
         
-        Servidor running on http://localhost:${this.port}
+        Server running on http://localhost:${this.port}
         Happy hacking! =)
         
       `);
     });
-  }
-}
+  };
+};
