b9ed2f119618efe505e89b18e7ea6979cd793fc2
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-21 04:24:08 -0300

    feat: get all projects controller

---
+++ backend/src/controllers/projects/getAllProjectsController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { prisma } from "../../config/prismaClient";
+
+export default async function getAllProjectsController(req: Request, res: Response) {
+    try {
+        const allProjects = await prisma.project.findMany();
+
+        return res.status(200).json({
+            status: "200 - Success",
+            data: !allProjects.length ? 'No projects found...' : allProjects
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: "500 - Internal server error",
+            message: "Something went wrong while fetching projects.",
+            error: error instanceof Error ? error.message : error
+        });
+    }
+}
