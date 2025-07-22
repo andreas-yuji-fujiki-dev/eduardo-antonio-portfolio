af1b1f80cfb4aa6a36e16d29bf9f55ca03a591a1
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-22 02:31:00 -0300

    feat: swagger stack schema

---
+++ backend/src/docs/components/stackSchema.ts
@@ -0,0 +1,17 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Stack:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         name:
+ *           type: string
+ *           example: "React"
+ *         logo:
+ *           type: string
+ *           example: "react-logo.svg"
+ */
