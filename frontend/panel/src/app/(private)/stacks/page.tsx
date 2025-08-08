22f81db183ddb8b6abc838d8c50981e030f71cb9
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-08-08 17:58:54 -0300

    feat: search input

--- frontend/panel/src/app/(private)/stacks/page.tsx
+++ frontend/panel/src/app/(private)/stacks/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+import PageContainer from "@/components/PageContainer";
+import CustomTitle from "@/components/CustomTitle";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
+
 export default function StacksManagementPage(){
+    const [ stackSearchValue, setStackSearchValue ] = useState<string>('');
+
+    function handleSearch(){
+        alert('Search')
+    };
+
     return(
-        <div>
-            
-        </div>
+        <PageContainer className="flex flex-col gap-4">
+            {/* page title */}
+            <CustomTitle variant="primary">
+                Stacks Management
+            </CustomTitle>
+
+            {/* search input */}
+            <div className="flex">
+                <CustomInput 
+                    value={ stackSearchValue }
+                    onChange={ (e) => setStackSearchValue(e.target.value) } 
+                    placeholder="Search for stacks"
+                    className="rounded-r-none border-r-0"
+                />
+                <CustomButton 
+                    variant="search"
+                    className="flex items-center rounded-l-none"
+                    onClick={() => handleSearch() }
+                />
+            </div>
+        </PageContainer>
     );
 };
