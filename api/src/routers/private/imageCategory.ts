5512d5a150047a3fd1b62eb05ceb782cd1cb04ce
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-09-26 02:51:11 -0300

    feat: image category router

---
+++ api/src/routers/private/imageCategory.ts
@@ -0,0 +1,44 @@
+// imports
+    // express
+    import { Router } from "express";
+
+    // middlewares
+    import authRequiredMiddleware from "../../middlewares/auth/authRequiredMiddleware";
+
+    // controllers
+    import getAllImageCategories from "../../controllers/imageCategory/getAllImageCategories";
+
+
+// router config
+    // definition
+    const imageCategoriesRouter = Router()
+    
+    // routes register
+        // get all
+        imageCategoriesRouter.get('/',
+            authRequiredMiddleware,
+            getAllImageCategories
+        )
+
+        // get by id
+        imageCategoriesRouter.get('/:id',
+            authRequiredMiddleware,
+        )
+
+        // register/create
+        imageCategoriesRouter.post('/',
+            authRequiredMiddleware,
+        )
+
+        // update/edit
+        imageCategoriesRouter.put('/',
+            authRequiredMiddleware,
+        )
+
+        // delete
+        imageCategoriesRouter.delete('/',
+            authRequiredMiddleware,
+        )
+
+// exporting router
+export default imageCategoriesRouter;
