a29e4dd1788340fd616b81a9f32b808ae8da2ac3
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-09-26 02:52:13 -0300

    feat: error when jwt secret is not defined on enviroment variables

--- frontend/panel/src/middleware.ts
+++ frontend/panel/src/middleware.ts
@@ -3,7 +3,10 @@ import { jwtVerify } from 'jose';
 
 const PUBLIC_ROUTES = ['/login', '/register'];
 const DEFAULT_ROUTE = '/login';
-const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET || '');
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET not set in .env");
+}
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -17,8 +20,9 @@ export async function middleware(request: NextRequest) {
   if (!token) {
     return NextResponse.redirect(new URL(DEFAULT_ROUTE, request.url));
   }
-
+  
   try {
+    if(!JWT_SECRET) throw new Error("JWT Secret is not defined on frontend!")
     await jwtVerify(token, JWT_SECRET);
     return NextResponse.next();
   } catch (error) {
