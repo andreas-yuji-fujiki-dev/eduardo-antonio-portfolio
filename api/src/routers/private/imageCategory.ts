8e1401518888e4213960f73ebafb05b37b65f90b
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-09-27 20:50:10 -0300

    feat: implements middleware and controller for get by id route

--- api/src/routers/private/imageCategory.ts
+++ api/src/routers/private/imageCategory.ts
@@ -4,9 +4,11 @@
 
     // middlewares
     import authRequiredMiddleware from "../../middlewares/auth/authRequiredMiddleware";
+    import getImageCategoryByIdMiddleware from "../../middlewares/imagesCategory/getImageCategoryByIdMiddleware";
 
     // controllers
     import getAllImageCategories from "../../controllers/imageCategory/getAllImageCategoriesController";
+    import getImageCategoryByIdController from "../../controllers/imageCategory/getImageCategoryByIdController";
 
 
 // router config
@@ -18,36 +20,29 @@
         imageCategoriesRouter.get('/',
             authRequiredMiddleware,
             getAllImageCategories
-        )
+        );
 
         // get by id
         imageCategoriesRouter.get('/:id',
             authRequiredMiddleware,
-            /*
-                const { id } = req.params;
-                const imageCategoryBySpecificId = await prisma.imageCategory.findUnique({
-                    where: {
-                        id
-                    }
-                })
-            
-            */
-        )
+            getImageCategoryByIdMiddleware,
+            getImageCategoryByIdController
+        );
 
         // register/create
         imageCategoriesRouter.post('/',
             authRequiredMiddleware,
-        )
+        );
 
         // update/edit
         imageCategoriesRouter.put('/',
             authRequiredMiddleware,
-        )
+        );
 
         // delete
         imageCategoriesRouter.delete('/',
             authRequiredMiddleware,
-        )
+        );
 
 // exporting router
 export default imageCategoriesRouter;
