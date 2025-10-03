73fc9fd390e0d23181a69528bce09d0b9b8891b3
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-03 06:53:15 -0300

    feat: import and attach middlewares and controller for register/create and update/edit

--- api/src/routers/private/imageCategory.ts
+++ api/src/routers/private/imageCategory.ts
@@ -5,10 +5,14 @@
     // middlewares
     import authRequiredMiddleware from "../../middlewares/auth/authRequiredMiddleware";
     import getImageCategoryByIdMiddleware from "../../middlewares/imagesCategory/getImageCategoryByIdMiddleware";
+    import createImageCategoryMiddleware from "../../middlewares/imagesCategory/createImageCategoryMiddleware";
+    import UpdateImageCategoryMiddleware from "../../middlewares/imagesCategory/UpdateImageCategoryMiddleware";
 
     // controllers
     import getAllImageCategories from "../../controllers/imageCategory/getAllImageCategoriesController";
     import getImageCategoryByIdController from "../../controllers/imageCategory/getImageCategoryByIdController";
+    import createImageCategoryController from "../../controllers/imageCategory/createImageCategoryController";
+    import updateImageCategoryController from "../../controllers/imageCategory/updateImageCategoryController";
 
 
 // router config
@@ -32,11 +36,15 @@
         // register/create
         imageCategoriesRouter.post('/',
             authRequiredMiddleware,
+            createImageCategoryMiddleware,
+            createImageCategoryController
         );
 
         // update/edit
-        imageCategoriesRouter.put('/',
+        imageCategoriesRouter.put('/:id',
             authRequiredMiddleware,
+            UpdateImageCategoryMiddleware,
+            updateImageCategoryController
         );
 
         // delete
