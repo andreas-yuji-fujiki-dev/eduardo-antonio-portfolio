cf174aacb1238d507f6701949cb43817c1f25c82
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-03 22:02:43 -0300

    feat: create

---
+++ api/src/controllers/imageCategory/deleteImageCategoryController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { prisma } from "../../config/prismaClient";
+
+export default async function deleteImageCategoryController(req: Request, res: Response){
+    const { id } = req.params;
+    
+    try {
+        // deleting & success return message
+        const deletedImageCategory = await prisma.imageCategory.delete({ where: { id: Number(id) } });
+        
+        return res.status(200).json({
+            status: "200 - Success",
+            message: `Successfuly deleted the image category with id ${id}`,
+            data: `Deleted: ${deletedImageCategory}`
+        })
+
+    } catch (error) {
+        
+        // internal server error
+        return res.status(500).json({
+            status: "500 - Internal server error",
+            message: `An unexpected error ocurred while trying to delete image category with id ${id}`,
+            details: error?.message || String(error)
+        })
+    }
+}
