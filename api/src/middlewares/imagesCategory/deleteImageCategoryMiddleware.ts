d84c0ee77151192818c96ab44c85589f82d370f1
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-03 22:03:03 -0300

    feat: create

---
+++ api/src/middlewares/imagesCategory/deleteImageCategoryMiddleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { prisma } from "../../config/prismaClient";
+
+export default async function deleteImageCategoryMiddleware(req: Request, res: Response, next: NextFunction){
+    const { id } = req.params;
+
+    if(!id || typeof(Number(id)) !== 'number' ) return res.status(400).json({
+        status: "400 - Bad request",
+        message: "You must provide the category id on the request params."
+    });
+
+    try {
+        
+        // verify if image category exist
+        const imageCategory = await prisma.imageCategory.findUnique({ where: { id: Number(id) } });
+        
+        if( !imageCategory ) return res.status(404).json({
+            status: "404 - Not found",
+            message: `Impossible to find image category with id '${id}'`
+        });
+
+        next()
+
+    } catch (error) {
+        
+        // internal server error
+        return res.status(500).json({
+            status: "500 - Internal server error",
+            message: `An unexpected error ocurred while trying to delete the image with id ${id}.`,
+            details: error?.message || String(error)
+        })
+    }
+};
