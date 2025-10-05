a231600790a99b0f45837da97b8e1c2c10e83387
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-04 23:14:14 -0300

    feat: semantic message and error unknown type

--- api/src/controllers/imageCategory/getAllImageCategoriesController.ts
+++ api/src/controllers/imageCategory/getAllImageCategoriesController.ts
@@ -8,11 +8,13 @@ export default async function getAllImageCategoriesController(req: Request, res:
 
         return res.status(200).json({
             status: "200 - Success",
-            message: "Successfully got all the stacks",
-            data: !!allImageCategories.length ? allImageCategories : "No image categories found..."
+            message: !!allImageCategories.length 
+                ? "Successfully got all the stacks"
+                : "No image categories found...",
+            data: allImageCategories
         })
 
-    } catch ( error ) {
+    } catch ( error: unknown ) {
         // in case of server internal error
          return res.status(500).json({
             status: "500 - Internal server error",
