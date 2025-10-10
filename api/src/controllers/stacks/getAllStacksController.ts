5f90bb979dc9f32b63dabf7db3051bd85aeb7076
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-10 17:47:04 -0300

    refactor: get all stacks response

--- api/src/controllers/stacks/getAllStacksController.ts
+++ api/src/controllers/stacks/getAllStacksController.ts
@@ -1,23 +1,34 @@
 import { Request, Response } from "express";
 import { prisma } from "../../config/prismaClient";
 
-export default async function getAllStacksController(req:Request, res:Response){
+export default async function getAllStacksController(req: Request, res: Response) {
     try {
         // get all stacks
-        const allStacks = await prisma.stack.findMany();
-        
-        return res.status(200).json({
-            status: "200 - Success",
-            message: "Successfully got all the stacks",
-            data: !!allStacks.length ? allStacks : "No stacks found..."
+        const allStacks = await prisma.stack.findMany({
+            include: {
+                category: true,
+                logo: true,
+                projects: {
+                    select: {
+                        project: true
+                    }
+                }
+            }
         });
 
-    } catch ( error ) {
-        // in case of server internal error
+        // remove categoryId and logoId from each stack
+        const stacksWithoutIds = allStacks.map(({ categoryId, logoId, ...stack }) => stack);
+
+        return res.status(200).json({
+            status: "200 - Success",
+            data: stacksWithoutIds.length ? stacksWithoutIds : "No stacks found..."
+        });
+
+    } catch (error) {
         return res.status(500).json({
             status: "500 - Internal server error",
             error: "An unexpected error ocurred",
             details: error?.message || String(error)
-        })
-    };
-};
+        });
+    }
+}
