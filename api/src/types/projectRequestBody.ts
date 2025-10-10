985c4e6859d2d0fa68d3fd2e44de12cc645b845e
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-10 17:51:16 -0300

    feat: removing types for now

--- api/src/types/projectRequestBody.ts
+++
@@ -1,9 +0,0 @@
-export interface ProjectRequestBody {
-  name: string;
-  description: string;
-  more_info: string;
-  deploy_link: string;
-  repository_link: string;
-  imageIds: number[];
-  stackIds: number[]
-};
