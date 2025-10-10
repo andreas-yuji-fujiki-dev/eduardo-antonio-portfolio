d85c9f97edfdf37cac1bd5bece7e61819c0961f7
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-10 17:44:17 -0300

    feat: verify if a stack already exists with the provided name

--- api/src/controllers/stacks/createStackController.ts
+++ api/src/controllers/stacks/createStackController.ts
@@ -5,44 +5,52 @@ export default async function createStackController(req: Request, res: Response)
     const { name, experience, logoId } = req.body;
 
     try {
-        // verify if image exists
-        const imageExists = await prisma.image.findUnique({
-            where: { id: logoId },
+        // verify if a stack already exists with the given name
+        const existingStackWithSameName = await prisma.stack.findUnique({ where: { name: String(name) } });
+
+        if (existingStackWithSameName) return res.status(409).json({
+            status: "409 - Conflict",
+            message: `A stack already exists with the name '${name}'`
         });
 
-        if (!imageExists) {
+        // verify if image exists (if logoId was provided)
+        const imageExists = logoId && await prisma.image.findUnique({ where: { id: logoId } });
+
+        if (logoId && !imageExists) {
             return res.status(404).json({
                 status: "404 - Not Found",
                 message: `Image with id '${logoId}' does not exist`,
             });
-        }
+        };
 
         // create stack
         const newStack = await prisma.stack.create({
             data: {
                 name,
                 experience,
-                logo: {
-                    connect: { id: logoId },
-                },
+                ...(logoId !== undefined && { logo: { connect: { id: logoId } } }),
             },
-            include: {
+            include: { 
+                category: true, 
                 logo: true,
-            },
+                projects: true 
+            }
         });
+
+        // remove categoryId and logoId from response
+        const { categoryId, logoId: _logoId, ...stackWithoutIds } = newStack;
 
         return res.status(201).json({
             status: "201 - Created",
             message: "Stack created successfully",
-            data: newStack,
+            data: stackWithoutIds
         });
 
     } catch (error) {
-        // in case of server internal error
         return res.status(500).json({
             status: "500 - Internal server error",
             error: "An unexpected error ocurred",
             details: error?.message || String(error)
-        })
+        });
     }
 }
