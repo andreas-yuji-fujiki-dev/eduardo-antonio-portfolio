ac92aac6c9d2aeda27a5bdd96d07d333327badef
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-10 17:47:29 -0300

    refactor: get stack by id response data

--- api/src/controllers/stacks/getStackByIdController.ts
+++ api/src/controllers/stacks/getStackByIdController.ts
@@ -1,30 +1,44 @@
 import { Request, Response } from "express";
 import { prisma } from "../../config/prismaClient";
 
-export default async function getStackByIdController(req:Request, res:Response){
+export default async function getStackByIdController(req: Request, res: Response) {
     const { id } = req.params;
 
     try {
-        // getting specific project by id
-        const specificStack = await prisma.stack.findUnique({ where: { id: Number(id) }});
-        if( !specificStack ){
+        // getting specific stack by id
+        const specificStack = await prisma.stack.findUnique({ 
+            where: { id: Number(id) },
+            include: { 
+                logo: true, 
+                category: true,
+                projects: {
+                    select: {
+                        project: true,
+                    }
+                }
+            }
+        });
+        
+        if (!specificStack) {
             return res.status(404).json({
                 status: "404 - Not found",
                 message: `Cannot find stack with id '${id}'`
-            });
+            })
         };
+
+        // remove categoryId and logoId from response
+        const { categoryId, logoId, ...stackWithoutIds } = specificStack;
 
         return res.status(200).json({
             status: "200 - Success",
-            data: specificStack
+            data: stackWithoutIds
         });
 
     } catch (error) {
-        // in case of server internal error
         return res.status(500).json({
             status: "500 - Internal server error",
             error: "An unexpected error ocurred",
             details: error?.message || String(error)
-        })
+        });
     }
-};
+}
