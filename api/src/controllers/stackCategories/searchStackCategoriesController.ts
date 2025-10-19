536cb2a5aff37e722bbed5d2438b0eceb81e65b1
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-10-19 01:40:18 -0300

    fix: remove middleware validations from controllers

--- api/src/controllers/stackCategories/searchStackCategoriesController.ts
+++ api/src/controllers/stackCategories/searchStackCategoriesController.ts
@@ -1,14 +1,9 @@
 import { Request, Response } from "express";
 import { prisma } from "../../config/prismaClient";
 
-import validateString from "../../utils/validateString";
-
 export default async function searchStackCategoriesController(req: Request, res: Response) {
   try {
     const { q } = req.query;
-
-    const errorValidatingQuery = validateString('query', q, res);
-    if ( errorValidatingQuery ) return errorValidatingQuery;
     
     const formattedQuery = String(q).toLowerCase();
 
