679baed32b7490b52c13d384c3639c132a5e9bf0
EduardoAntonio77 <fantauva250ml@gmail.com>
2025-11-06 10:56:17 -0300

    feat:'Implement pagination for Image categories route'

--- api/src/controllers/imageCategory/getAllImageCategoriesController.ts
+++ api/src/controllers/imageCategory/getAllImageCategoriesController.ts
@@ -3,17 +3,36 @@ import { prisma } from "../../config/prismaClient";
 
 export default async function getAllImageCategoriesController(req: Request, res: Response) {
     try {
+
+        // pagination
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 5;
+        const skip = (page - 1) * limit;
+
         // get all
         const allImageCategories = await prisma.imageCategory.findMany({
+            skip,
+            take: limit,
             include: {
                 images: true
             }
         });
 
+      // total number of existing categories images
+        const totalImagesCategories = await prisma.imageCategory.count();
+
         // success
         return res.status(200).json({
             status: "200 - Success",
             message: "Successfully got all image categories",
+            pagination: {
+                currentPage: page, limit,
+                totalItems: totalImagesCategories,
+                totalPages: Math.ceil( totalImagesCategories / limit ),
+                hasPrevPage: page < 1,
+                hasNextPage: (page * limit) < totalImagesCategories
+                
+            },
             data: !!allImageCategories.length 
                 ? allImageCategories 
                 : "No image categories found..."
